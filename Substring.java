b2bc4e430d6fcc3161e673428d7eacb069fbace8
Pradeep Ranjan <pradeep.ranjan7837@gmail.com>
2022-09-12 23:32:16 -0400

    Prog: Get any substring from string

---
+++ Substring.java
@@ -0,0 +1,10 @@
+public class Substring {
+    public static void main(String[] args) {
+
+        String s = "Hello world";
+
+        String subs = s.substring(0,5);
+        System.out.println(subs);
+
+    }
+}
